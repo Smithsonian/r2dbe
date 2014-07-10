359b724488b050bfb6768dc1ccfdd131d9d23802
Rurik A Primiani <rprimian@cfa.harvard.edu>
2014-07-10 14:04:51 -0400

    [software] Created a software sub-directory and added a basic VDIF receiver.
    This script will capture N number of UDP packets and print out the entire
    VDIF header as well as parts of the beginning and end of the data. To run
    use the following command:
    
    $ python ./software/vdif_recv.py <N>
    
    Where <N> should be replaced by the numer of packets you'd like to grab.
    Note: this has the IP hard-coded and will only work of course if you have
    a 10 GbE card connected directly to the ROACH2 running the R2DBE bitcode.

---
+++ software/vdif_recv.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python2.7
+import sys, socket, struct
+
+UDP_IP = "192.168.10.30"
+UDP_PORT = 4001
+
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+sock.bind((UDP_IP, UDP_PORT))
+
+for i in range(int(sys.argv[1])):
+    pkt, addr = sock.recvfrom(8224) # buffer size is 8224 bytes
+    header = struct.unpack('>8I', pkt[:32])
+    data = struct.unpack('>%dI' % (len(pkt[32:])/4), pkt[32:])
+
+    print "received packet %d" % i
+    print "received header: "
+    for h in range(8):
+        print "{:4d}: 0x{:08X}".format(h, header[h])
+    print
+    print "received data: "
+    for d in range(8):
+        print "{:4d}: 0x{:08X}".format(d, data[d])
+    print ". . . . . . . ."
+    for d in range(len(data)-8, len(data)):
+        print "{:4d}: 0x{:08X}".format(d, data[d])
+    print 
+    print
