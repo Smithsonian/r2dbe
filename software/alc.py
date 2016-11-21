4279891d9f0ec33b34773b5c4786b18efa924f26
anyoung <andre.young@cfa.harvard.edu>
2016-11-21 21:02:02 +0000

    Define threshold compute function
    The threshold value is now computed in a defined method that can be
    imported from within other python source files.

--- software/alc.py
+++ software/alc.py
@@ -1,5 +1,19 @@
 import adc5g, corr
 
+def get_th_16_84(x):
+    # sort data
+    L = len(x8)
+    y = sorted(x8)
+    # find value 16% of the way through
+    Lt = int(L*0.16)
+    th1 = abs(y[Lt-1])
+    # find value at 84% of the way through
+    Lt2 = int(L*0.84)
+    th2 = abs(y[Lt2-1])
+    # average these threshold values
+    th = (th1+th2)/2
+    return th
+
 import argparse
 parser = argparse.ArgumentParser(description='Set 2-bit quantization threshold')
 parser.add_argument('-t','--timeout',metavar='TIMEOUT',type=float,default=5.0,
@@ -21,39 +35,16 @@ if args.verbose > 1:
     print "connected to '{0}'".format(args.host)
 
 for ii in ['0','1']:
-   # grab snapshot of 8 bit data from each input
-   if args.verbose > 2:
-       print "get snapshot form 'r2dbe_snap_8bit_{0}_data'".format(ii)
-   x8 = adc5g.get_snapshot(roach2,'r2dbe_snap_8bit_'+ii+'_data')
+    # grab snapshot of 8 bit data from each input
+    if args.verbose > 2:
+        print "get snapshot form 'r2dbe_snap_8bit_{0}_data'".format(ii)
+    x8 = adc5g.get_snapshot(roach2,'r2dbe_snap_8bit_'+ii+'_data')
+    
+    th = get_th_16_84(x8)
 
-   # sort data
-   L    = len(x8)
-   y    = sorted(x8)
-
-   # find value 16% of the way through
-   Lt   = int(L*0.16)
-   th1   = abs(y[Lt-1])
-   if args.verbose > 1:
-       print "th{0}_1 = {1}".format(ii,th1)
-
-   # find value at 84% of the way through
-   Lt2  = int(L*0.84)
-   th2  = abs(y[Lt2-1])
-   if args.verbose > 1:
-       print "th{0}_2 = {1}".format(ii,th2)
-
-   # average these threshold values
-   th   = (th1+th2)/2
-   if args.verbose > 0:
-       print "threshold {0} = {1}".format(ii,th)
-
-   # write threshold to FPGA
-   if args.verbose > 2:
-       print "write threshold to 'r2dbe_quantize_{0}_thresh'".format(ii)
-   roach2.write_int('r2dbe_quantize_'+ii+'_thresh', th)
-
-   
-
-
-
-
+    if args.verbose > 0:
+        print "threshold {0} = {1}".format(ii,th)
+    # write threshold to FPGA
+    if args.verbose > 2:
+        print "write threshold to 'r2dbe_quantize_{0}_thresh'".format(ii)
+    roach2.write_int('r2dbe_quantize_'+ii+'_thresh', th)
