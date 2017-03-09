8f095312aa4e8e3f510f97a9034113a7d4a1ce25
anyoung <andre.young@cfa.harvard.edu>
2017-03-09 04:22:44 +0000

    Update software for 8pac compatibility and threshold read disable
    Data grabbed off the network needs to be processed from VDIF-over-8pac
    to VDIF before it can be analyzed.
    
    The threshold reading option is no longer supported in the histogram
    plotting script.

--- software/get_data_plot_hist.sh
+++ software/get_data_plot_hist.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-./grab_consec.sh get_data 2048 eth2
-./grab_consec.sh get_data 2048 eth3
-./grab_consec.sh get_data 2048 eth4
-./grab_consec.sh get_data 2048 eth5
+for ii in {2..5} ; do
+	./grab_consec.sh get_data_8pac 256 eth${ii}
+done
 
-python plot_hist_corrs.py -v -t -gl 800 -gh 14000 get_data*vdif
+for ii in {2..5} ; do
+	python vdif_8pac_to_vdif.py < get_data_8pac_eth${ii}.vdif > get_data_eth${ii}.vdif
+done
 
-rm get_data_eth2.vdif
-rm get_data_eth3.vdif
-rm get_data_eth4.vdif
-rm get_data_eth5.vdif
+python plot_hist_corrs.py -v -gl 800 -gh 14000 get_data_eth?.vdif
+
+rm get_data*_eth?.vdif
+
+
